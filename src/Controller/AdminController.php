bbc7f0caa098fcf6f76d48db3bfed3ed520257b6
HediKhemiri <khemirimedhedi@gmail.com>
2021-06-11 15:16:23 +0100

    Changed AdminController to get all reservations, User profile now gives all reservations made by User

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Reservation;
 use App\Entity\Salle;
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,52 +13,17 @@ class AdminController extends AbstractController
 {
     /**
      *
-     * @Route("/admin/users", name="admin_users_show")
+     * @Route("/admin/ReservationsList", name="admin_users_show")
      *
      */
-    function getUsers(){
-        //Getting only verified users.
-        $users = $this->getDoctrine()
-            ->getRepository(User::class)
-            ->findBy(['isVerified' => 1]);
-        //Getting all salles.
-        $salles = $this->getDoctrine()
-            ->getRepository(Salle::class)
+    function getReservations(){
+        //Getting all reservations.
+        $reservations = $this->getDoctrine()
+            ->getRepository(Reservation::class)
             ->findAll();
-        return $this->render('admin/showUserGroup.html.twig',
-            ['users' => $users,
-            'salles' => $salles,
-            'numbersalles'=> count($salles)]);
+        return $this->render('admin/showAllReservations.html.twig',
+            ['reservations' => $reservations]);
     }
-    /**
-     *
-     * @Route("/admin/user{id}", name="admin_user_show")
-     *
-     */
-    function getUserById(int $id){
-        //Getting the user with id = $id.
-        $user = $this->getDoctrine()
-            ->getRepository(User::class)
-            ->find($id);
-        //Getting all salles.
-        $salles = $this->getDoctrine()
-            ->getRepository(Salle::class)
-            ->findAll();
-        //If user with id = $id does not exist :
-        if (!$user) {
-            return $this->render('admin/NoUser.html.twig',['id' => $id]);
-        }
-        //If user does exist:
-        return $this->render('admin/showUser.html.twig',
-            ['id' => $id,
-            'fullname' => $user->getFullName(),
-            'email' => $user->getEmail(),
-            'salles' => $salles,
-            'numbersalles'=> count($salles),
-            'is_verified' => $user->isVerified()]);
-    }
-
-
 
     #[Route('/admin', name: 'admin')]
     public function index(): Response
